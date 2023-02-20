81e6a5b7b513545856b5b127f21cd1ff3ef414ac
lengel.igor@gmail.com <FcBarcelona123>
2023-02-20 22:57:42 +0100

    app

--- MockupApp/src/main/java/org/example/Main.java
+++ MockupApp/src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    private final String outFileName = "object.json";
+    private static final String outFileName = "object.json";
     public static void main(String[] args) {
+        List<TestDto> testDtoList = new ArrayList<>();
 
+        for (int i = 1; i <= 5; i++){
+            TestDto testDto = new TestDto();
+            testDto.setFirstName("Firstname" + i);
+            testDto.setLastName("Lastname" + i);
+            testDto.setYear(20 + i);
+            testDtoList.add(testDto);
+        }
+
+        ObjectMapper obj = new ObjectMapper();
+        try {
+            String jsonStr = obj.writeValueAsString(testDtoList);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outFileName));
+            writer.write(jsonStr);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ MockupApp/src/main/java/org/example/TestDto.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class TestDto {
+
+    private String firstName;
+    private String lastName;
+    private int year;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
